177a1832836d7819744c6a778c2cbd696497379e
SleepyWindows <calvillo.edwin@gmail.com>
2020-07-08 11:03:30 -0500

    submission

--- src/App.js
+++ src/App.js
@@ -4,14 +4,53 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+  state = {
+    poems: null,
+    displayPoems: null,
+    isLoading: false,
+    show: false
+  }
+  componentDidMount(){
+  fetch('http://localhost:6001/poems')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    this.setState({poems: data, displayPoems: data, isLoading: true  })
+    console.log(this.state)
+  })
+}
+
+addPoem = (e) =>{
+  console.log(e.target.title.value)
+  fetch('http://localhost:6001/poems', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' 
+    },
+    body:JSON.stringify(
+      {
+        title: e.target.title.value,
+        author: e.target.author.value,
+        content: e.target.content.value
+      })
+  })
+  .then(res => res.json())
+  .then(result => {
+    this.setState({displayPoems: [...this.state.displayPoems, result]})
+  })
+}
   render() {
+    if(!this.state.isLoading){
+      return <h3>Loading...</h3>
+    }
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={() =>{this.setState({show: !this.state.show})}}>Show/hide new poem form</button>
+          {this.state.show? <NewPoemForm addPoem={this.addPoem}/>
+          : null}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.displayPoems}/>
       </div>
     );
   }
--- src/Poem.js
+++ src/Poem.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 class Poem extends React.Component {
-  render() {
+  state = {
+    readStatus: false
+  }
+  render() {console.log(this.props)
     return (
       <div>
-        <h3>Title</h3>
-        <p>Content</p>
+        <h3>{this.props.sinPoem.title}</h3>
+        <p>{this.props.sinPoem.content}</p>
         <p>
-          <strong>- By Author</strong>
+        <strong>-{this.props.sinPoem.author}</strong>
         </p>
-        <button>Mark as read</button>
+        <button onClick={() => this.setState({readStatus: !this.state.readStatus})}>{!this.state.readStatus? 'Mark as Read' : 'Mark as Unread'}</button>
       </div>
     );
   }
