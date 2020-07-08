b578472708cca37bd687078f11217395425292ce
tedluu <luuphanbaothach@gmail.com>
2020-07-08 12:05:51 -0400

    m

--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,12 +1,46 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      author: "",
+      content: "",
+      
+    }
+
+  }
+
+  handleTitle = event => {
+    this.setState({
+      title: event.target.value
+    })
+
+  }
+  handleAuthor = event => {
+    this.setState({
+      author: event.target.value
+    })
+
+  }
+  handleContent = event => {
+    this.setState({
+      content: event.target.value
+    })
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.postPoem(this.state)
+  }
   render() {
     return (
-      <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
+      <form className="new-poem-form" onSubmit = {event => this.handleSubmit(event)}>
+        <input placeholder="Title" onChange = {event => this.handleTitle(event)} value={this.state.title}/>
+        <input placeholder="Author" onChange = {event => this.handleAuthor(event)}/>
+        <textarea placeholder="Write your masterpiece here..." rows={10} onChange = {event => this.handleContent(event)} />
         <input type="submit" value="Share your masterpiece" />
       </form>
     );
--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -2,11 +2,39 @@ import React from "react";
 import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     poems: [],
+      
+  //   }
+
+  // }
+
+  // componentDidMount() {
+  //   fetch("http://localhost:6001/poems/")
+  //     .then(res => res.json())
+  //     .then(data =>
+  //       this.setState({
+  //         poems: data
+  //       }))
+  // }
+
+  // postPoem(poemObj) {
+  //   const newPoem = {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(poemObj)
+  //   }
+  //   fetch("http://localhost:6001/transactions/", newPoem)
+  // }
   render() {
     return (
       <div className="poems-container">
         {
-          // render poems here
+        this.props.poems.map(poem => (<Poem poem={poem} key={poem.id}/>))  // render poems here
         }
       </div>
     );
